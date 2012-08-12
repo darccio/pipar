f20bbae3c6fcc263b2c9fd3dfc09f1cc399b2661
Dario Castañé <i@dario.im>
2012-08-12 18:12:19 +0200

    Metadata support done.

--- examples/mir.es.rb
+++ examples/mir.es.rb
@@ -9,6 +9,24 @@ Configuration.for('pipar') {
   data_file  "#{self.data_dir}/parties.json"
   url        'https://servicio.mir.es/nfrontal/webpartido_politico.html'
   page_url   'https://servicio.mir.es/nfrontal/webpartido_politico/partido_politicoBuscar.html?pagActual=%{page}'
+
+  # Data metadata
+  __metadata__ = {
+    'organization' => {
+      'name' => 'Ministerio del Interior de España',
+      'href' => 'http://www.interior.gob.es/',
+      'source' => {
+        'name' => 'Registro de Partidos Políticos',
+        'href' => self.url
+      }
+    },
+    'license' => {
+      'name' => 'CC0 1.0 Universal (CC0 1.0)',
+      'href' => 'http://creativecommons.org/publicdomain/zero/1.0/'
+    }
+  }
+  metadata __metadata__
+
   # Partial download support.
   start_page 0
   skip_to    0
